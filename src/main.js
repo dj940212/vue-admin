69a19f9613d41da8e1fc056af2c81141b521cb0c
dj940212 <2902273280@qq.com>
2017-03-30 17:00:09 +0800

    引入element-ui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 
 Vue.config.productionTip = false
 
