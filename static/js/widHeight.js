e6ae5fda040a498f62aa290fa5cb32797ac04c0c
Your Name <you@example.com>
2017-05-07 18:09:27 +0800

    优化性能

--- static/js/widHeight.js
+++ static/js/widHeight.js
@@ -3,17 +3,19 @@ window.onload = function(){
   document.getElementById('routerView').style.height = (window.innerHeight-70)+"px";
 
   document.getElementById('app').style.height = window.innerHeight + "px";
-  document.getElementById('app').style.height = window.innerWidth + "px";
+  document.getElementById('app').style.width = window.innerWidth + "px";
+
+  document.getElementById('sidebar').style.height = "100%";
 
 }
 window.onresize=function() {
-        document.getElementById('routerView').style.width = (window.innerWidth-180)+"px";
-        document.getElementById('routerView').style.height = (window.innerHeight-70)+"px";
-        document.getElementById('app').style.height = window.innerHeight + "px";
-        document.getElementById('app').style.width = window.innerWidth + "px";
+      document.getElementById('routerView').style.width = (window.innerWidth-180)+"px";
+      document.getElementById('routerView').style.height = (window.innerHeight-70)+"px";
+      document.getElementById('app').style.height = window.innerHeight + "px";
+      document.getElementById('app').style.width = window.innerWidth + "px";
 
 
-        // document.getElementById('tab').style.width = (window.innerHeight-290)+"px";
-        document.getElementById('sidebar').style.height = "100%";
+      // document.getElementById('tab').style.width = (window.innerHeight-290)+"px";
+      document.getElementById('sidebar').style.height = "100%";
 
 }
